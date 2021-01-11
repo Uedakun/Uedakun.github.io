b3dc308f3caa97356b1e45d21f95b4a0d8f8b564
上田くん <52699897+Uedakun@users.noreply.github.com>
2021-01-11 19:53:05 +0900

    Delete sc.js

--- sc.js
+++
@@ -1,8 +0,0 @@
-// Scrapbox redirect rule
-   app.get(/\/scrapbox\/(.+)/, (req, res) => {
-     logger.info('on /scrapbox req.params', req.params);
-     // Get Scrapbox title
-     const title: string = req.params[0];
-     // Redirect to Scrapbox
-     res.redirect(301, `https://scrapbox.io/Ueda-desk/${encodeURIComponent(title)}`);
-   });
